f946524e73599883ea36b498611296b755154ea3
Sergio Medeiros <sergiof11@outlook.com.br>
2022-09-08 17:09:33 -0300

    list itens align

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import {useState} from 'react';
 
 function App() {
   const [showButtonText,setShowButtonText] = useState(false)
+  const [isMenuOpen,setIsMenuOpen] = useState(false)
   const altera_tamanho = () => {
-    
+    setIsMenuOpen(!isMenuOpen)
+
     if (!showButtonText) {
       setTimeout(() => {
         setShowButtonText(!showButtonText)
@@ -17,18 +19,26 @@ function App() {
         
       },200)
     }
-  
+    
+
 
     const x = document.getElementById('div')
     const menuIcon = document.getElementById('teste')
+    const menuIcon2 = document.getElementById('teste2')
+    const listItem = document.getElementById('listItem')
+
     if(x.style.width === "180px"){
+        // listItem.style.justifyContent = "flex-start"
         x.style.width = 70 + 'px'
         menuIcon.style.visibility = 'hidden'
+        menuIcon2.style.visibility = 'hidden'
     } else {
+        // listItem.style.justifyContent = "center"
         x.style.width = 180 + 'px'
 
        setTimeout(() => {
         menuIcon.style.visibility = 'visible'
+        menuIcon2.style.visibility = 'visible'
        },500)
     }
 
@@ -44,15 +54,20 @@ function App() {
             </div>
             
             <ul className="list">
-                <li><img src="./images/search.svg" alt=""/> {showButtonText && "Buscar"}</li>
-                <li><img src="./images/grid.svg" alt=""/>{showButtonText && "Dashboard"}</li>
-                <li><img src="./images/pet.svg" alt=""/> {showButtonText && "Pets"}</li>
-                <li><img src="./images/user.svg" alt=""/>{showButtonText && "Clientes"}</li>
-                <li><img src="./images/vet.svg" alt=""/> {showButtonText && "Vets"}</li>
-                <li><img src="./images/settings.svg" alt=""/> {showButtonText && "Ajustes"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/search.svg" alt=""/> {isMenuOpen && "Buscar"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/grid.svg" alt=""/>{isMenuOpen && "Dashboard"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/pet.svg" alt=""/> {isMenuOpen && "Pets"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/user.svg" alt=""/>{isMenuOpen && "Clientes"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/vet.svg" alt=""/> {isMenuOpen && "Vets"}</li>
+                <li style={{justifyContent: isMenuOpen ? "flex-start" : "center"}}><img src="./images/settings.svg" alt=""/> {isMenuOpen && "Ajustes"}</li>
             </ul>
+            <div className='divMeio'></div>
+            
+            <button id='teste2'><img src="./images/photo.jpg" alt="" />Veterinario Sergel</button>
+            <div id='divButton2'>
+            <button id="buttonAction2" onClick={altera_tamanho}><img src="./images/log-out.svg" alt=""/></button>
+            </div>
         </div>
-        <div className="dashBoard">swswsw</div>
      
     </div>
     </div>
